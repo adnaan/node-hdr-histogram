0ad3c05b2c52254d447a9d30c799e7b9f7ec414e
Abraham Kiggundu <abraham@thekiggys.com>
2016-01-28 22:21:25 +0000

    Completed skeleton index.js and added node-java dependency

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # node-hdr-histogram
 A port of HDR Histogram at http://hdrhistogram.github.io/HdrHistogram for use with node
+
+##Build
+- Ensure that JAVA_HOME is set to a JDK (not JRE) and that JDK is set up to support JNI. [see
+here](https://github.com/joeferner/node-java/issues/90#issuecomment-45613235) for details
---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by abrahamkiggundu on 28/01/2016.
+ */
+
+module.exports = function (highestTrackableValue, numberOfSignificantValueDigits) {
+    return {
+        recordValue: function(value){},
+        reset: function(){},
+        outputPercentileDistribution: function(filePath, outputValueUnitScalingRatio){}
+    }
+};
